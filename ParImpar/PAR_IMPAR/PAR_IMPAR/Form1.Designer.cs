8bc1d4b951f68a4ec3a39b0a58c46eba088715a8
AJuniorGarcia <124395333+AJuniorGarcia@users.noreply.github.com>
2023-02-04 05:45:39 +0100

    Add files via upload

---
+++ ParImpar/PAR_IMPAR/PAR_IMPAR/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PAR_IMPAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numerotextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.verificarbutton1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numerotextBox1
+            // 
+            this.numerotextBox1.Location = new System.Drawing.Point(356, 178);
+            this.numerotextBox1.Name = "numerotextBox1";
+            this.numerotextBox1.Size = new System.Drawing.Size(382, 26);
+            this.numerotextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingres el número:";
+            // 
+            // verificarbutton1
+            // 
+            this.verificarbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verificarbutton1.Location = new System.Drawing.Point(313, 341);
+            this.verificarbutton1.Name = "verificarbutton1";
+            this.verificarbutton1.Size = new System.Drawing.Size(172, 49);
+            this.verificarbutton1.TabIndex = 2;
+            this.verificarbutton1.Text = "Verificar";
+            this.verificarbutton1.UseVisualStyleBackColor = true;
+            this.verificarbutton1.Click += new System.EventHandler(this.verificarbutton1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(468, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 28);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 28);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(96, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(730, 64);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ingrese un valor entero y devuelve si es par o impar, \r\n                         " +
+    "positivo o negativo.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.verificarbutton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numerotextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numerotextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button verificarbutton1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ParImpar/PAR_IMPAR/PAR_IMPAR/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAR_IMPAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void verificarbutton1_Click(object sender, EventArgs e)
+        {
+            if (numerotextBox1.Text == "")
+            {
+                errorProvider1.SetError(numerotextBox1, "Ingrese un valor");
+                return; 
+            }
+            errorProvider1.Clear();
+           decimal numero = Convert.ToDecimal(numerotextBox1.Text);
+            PosiNegv(numero);//funcion para verificar si es  Positivo o negativo
+            ParImpar(numero);//funcion para verificar par o impar
+        }
+        private void PosiNegv(decimal numero)
+        {
+            if (numero > 0)
+            {
+                label3.Text = "El  número es positivo";
+            }
+            else if (numero < 0)
+            {
+                label3.Text = "El  número es negativo";
+            }
+            else
+            {
+                label3.Text = "El  número es cero";
+            }
+        }
+        private void ParImpar(decimal numero)
+        {
+            if (numero % 2 == 0)
+            {
+                label2.Text = "El número es par";
+            }
+            else
+            {
+                label2.Text = "El número es impar";
+            }
+
+        }
+
+    }
+}
