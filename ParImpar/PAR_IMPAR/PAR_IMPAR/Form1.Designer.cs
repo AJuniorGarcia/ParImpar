90fec3b7c1ea0531a819edded95c98a070c421cc
YaniorMunguia <111203108+YaniorMunguia@users.noreply.github.com>
2023-04-25 10:18:30 -0600

    cambio

--- ParImpar/PAR_IMPAR/PAR_IMPAR/Form1.Designer.cs
+++ ParImpar/PAR_IMPAR/PAR_IMPAR/Form1.Designer.cs
@@ -36,32 +36,36 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // numerotextBox1
             // 
-            this.numerotextBox1.Location = new System.Drawing.Point(356, 178);
+            this.numerotextBox1.Location = new System.Drawing.Point(237, 116);
+            this.numerotextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numerotextBox1.Name = "numerotextBox1";
-            this.numerotextBox1.Size = new System.Drawing.Size(382, 26);
+            this.numerotextBox1.Size = new System.Drawing.Size(256, 20);
             this.numerotextBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(97, 174);
+            this.label1.Location = new System.Drawing.Point(65, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 29);
+            this.label1.Size = new System.Drawing.Size(149, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingres el número:";
             // 
             // verificarbutton1
             // 
             this.verificarbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.verificarbutton1.Location = new System.Drawing.Point(313, 341);
+            this.verificarbutton1.Location = new System.Drawing.Point(209, 222);
+            this.verificarbutton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.verificarbutton1.Name = "verificarbutton1";
-            this.verificarbutton1.Size = new System.Drawing.Size(172, 49);
+            this.verificarbutton1.Size = new System.Drawing.Size(115, 32);
             this.verificarbutton1.TabIndex = 2;
             this.verificarbutton1.Text = "Verificar";
             this.verificarbutton1.UseVisualStyleBackColor = true;
@@ -75,42 +79,56 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(468, 252);
+            this.label2.Location = new System.Drawing.Point(312, 164);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 28);
+            this.label2.Size = new System.Drawing.Size(0, 19);
             this.label2.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(97, 252);
+            this.label3.Location = new System.Drawing.Point(65, 164);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 28);
+            this.label3.Size = new System.Drawing.Size(0, 19);
             this.label3.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(96, 47);
+            this.label4.Location = new System.Drawing.Point(64, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(730, 64);
+            this.label4.Size = new System.Drawing.Size(507, 48);
             this.label4.TabIndex = 5;
             this.label4.Text = "Ingrese un valor entero y devuelve si es par o impar, \r\n                         " +
     "positivo o negativo.";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(442, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "hola";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 450);
+            this.ClientSize = new System.Drawing.Size(619, 292);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.verificarbutton1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numerotextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
@@ -128,6 +146,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
